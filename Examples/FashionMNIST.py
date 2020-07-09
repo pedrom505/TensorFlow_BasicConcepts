850a3809ea01075fa24c21b7730b3156a23660ee
pedro.pereira <pedro.pereira@bematech.com.br>
2020-07-09 19:29:11 -0300

    finished the example

--- Examples/FashionMNIST.py
+++ Examples/FashionMNIST.py
@@ -1,12 +1,41 @@
-# Importing Tensorflow and Keras
+import os
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-
-# Imported the module numpy to enable numerial computing with Python
-import numpy as np
-
-# Imported the module matplotlib to plot all charts
 import matplotlib.pyplot as plt
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100*np.max(predictions_array),
+                                         class_names[true_label]),
+               color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array, true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
 
 # importing the dataset fashion MNIST
 fashion_mnist_dataset = keras.datasets.fashion_mnist
@@ -25,7 +54,7 @@ print(f"The training set has {train_images.shape[0]} images with {train_images.s
 print(f"The testing set has {test_images.shape[0]} images with {test_images.shape[1]} x {test_images.shape[2]} pixels")
 
 
-show_image = True
+show_image = False
 while show_image:
     image_number = input("Type the number of imagem to be shown (type \'q\' to jump this process):")
     if image_number.isnumeric():
@@ -80,5 +109,30 @@ model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
+
+# The next step is train my model using the function fit in the model and passing as parameter the training dataset and
+# the number of the epochs that will be used to train the model
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
+
+probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+
+predictions = model.predict(test_images)
+
+
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions[i], test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
 
 
